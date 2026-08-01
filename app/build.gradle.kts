50c60d3251b32726ac7ae09cfc07e28dde2cea80
Hotaro <hotaro@example.com>
2026-08-01 12:49:08 +0530

    Bump version to v4.0 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,8 +95,8 @@ android {
         applicationId = "com.material.downloader"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "3.9"
+        versionCode = 6
+        versionName = "4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
 
